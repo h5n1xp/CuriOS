da7891d2c267ff66a2fafb7da6a9204b1201f0e9
h5n1xp <h5n1xp@gmail.com>
2021-08-11 17:07:21 +0100

    Add files via upload

--- ExampleProgram/curios/intuition.h
+++ ExampleProgram/curios/intuition.h
@@ -50,11 +50,11 @@ struct window_t{
     uint64_t flags;
     messagePort_t* eventPort;
     bool focused;
+    bool isVisible;
     bool needsRedraw;
     bool noRise;             //no clicking to front
     bool isBusy;             //window busy, show busy pointer and should not receive events
     bitmap_t* bitmap;
-    int32_t autoRefeshCountdown;
     list_t gadgetList;
     
     lock_t clipRectsLock;   //always lock the cliprects list before using.
@@ -94,24 +94,44 @@ struct gadget_t{
 };
 
 
-#define WINDOW_EVENT_RESIZE     0x1
-#define WINDOW_EVENT_RESIZE_END 0x2
-#define WINDOW_EVENT_KEYDOWN    0x4
-#define WINDOW_EVENT_CLOSE      0x8
-#define WINDOW_EVENT_VSYNC      0x10
+#define WINDOW_EVENT_RESIZE                 0x1
+#define WINDOW_EVENT_RESIZE_END             0x2
+#define WINDOW_EVENT_KEYDOWN                0x4
+#define WINDOW_EVENT_CLOSE                  0x8
+#define WINDOW_EVENT_VSYNC                  0x10
+#define WINDOW_EVENT_REQUEST_OPEN_WINDOW    0x20  //when received by intuition, will enqueue the window, event data points to windowList
+#define WINDOW_EVENT_REQUEST_CLOSE_WINDOW   0x40  //when received by intuition, will remove the window
+#define WINDOW_EVENT_REQUEST_RESIZE_WINDOW  0x80  //when received by intuition, will resize the window
+#define WINDOW_EVENT_REQUEST_DRAW_TO_WINDOW 0x100 //When received by intutuion, will draw to window in intuition's context
+#define WINDOW_EVENT_REQUEST_CHANGE_VISIBILITY 0x200
 
+#define WINDOW_DRAW_COMMAND_PLOT        0x1
+#define WINDOW_DRAW_COMMAND_CLEAR       0x2
+#define WINDOW_DRAW_COMMAND_PUTCHAR     0x3
+#define WINDOW_DRAW_COMMAND_STRING      0x4
+#define WINDOW_DRAW_COMMAND_RECTANGLE   0x5
+#define WINDOW_DRAW_COMMAND_LINE        0x6
+#define WINDOW_DRAW_COMMAND_FILL        0x7
+#define WINDOW_DRAW_COMMAND_CIRCLE      0x8
+#define WINDOW_DRAW_COMMAND_TRIANGLE    0x9
+#define WINDOW_DRAW_COMMAND_VECTOR      0xA
+
+#define WINDOW_DRAW_FLAG_NOFILL         0x0
+#define WINDOW_DRAW_FLAG_FILL           0x1
+
+// the event stucture is overloaded at the moment for draw commands, I will define a special event structure for this in future.
 typedef struct{
     message_t   message;
     uint64_t    flags;
     window_t*   window;
-    gadget_t*   gadget;
-    void*       data;       // pointer to extra data
-    uint8_t     rawKey;
-    uint8_t     scancode;
-    int32_t     mouseX;
-    int32_t     mouseY;
-    int32_t     mouseXrel;
-    int32_t     mouseYrel;
+    gadget_t*   gadget;     // string pointer
+    void*       data;       // draw colour
+    uint8_t     rawKey;     // draw command
+    uint8_t     scancode;   // draw flag
+    int32_t     mouseX;     // x1
+    int32_t     mouseY;     // y1
+    int32_t     mouseXrel;  // x2   // r
+    int32_t     mouseYrel;  // y2
 }intuitionEvent_t;
 
 #define WINDOW_DRAGGABLE    0x1
@@ -121,7 +141,7 @@ typedef struct{
 #define WINDOW_TITLEBAR     0x10
 #define WINDOW_RESIZABLE    0x20
 #define WINDOW_BORDERLESS   0x40
-#define WINDOW_VSYNC        0x80    // With this flag the window will receive VSYNC events
+#define WINDOW_VSYNC        0x80    // With this flag the window will be sent VSYNC events
 
 
 
@@ -186,6 +206,7 @@ typedef struct{
     void (*PriorityOrderPrivate)(window_t* window);
     void (*Focus)(window_t* window);
     void (*SetBusy)(window_t* window, bool state);
+    void (*SetVisible)(window_t* window, bool state);
     void (*Plot)(window_t* window,uint32_t x, uint32_t y, uint32_t colour);
     void (*ClearWindow)(window_t*);
     void (*PutChar)(window_t*, uint32_t x, uint32_t y, uint8_t character, uint32_t fColour, uint32_t bColour);
@@ -198,44 +219,19 @@ typedef struct{
 
     void (*DrawVectorImage)(window_t* window, uint32_t x, uint32_t y, uint8_t* data);
     
-    void (*RedrawWindow)(window_t* window);
+    void (*updateLayers)(window_t* window);
     gadget_t* (*CreateGadget)(window_t* window,uint32_t flags);
     window_t* (*Request)(char* title);
+    
+    //Private functions may be removed
+    window_t* (*OpenWindowPrivate)(window_t* parent,uint32_t x, uint32_t y, uint32_t w, uint32_t h,uint64_t flags,char* title);
+    
+    list_t* windowList;
+    messagePort_t* intuiPort;
 } intuition_t;
 
 extern intuition_t intuition;
 
 void LoadIntuitionLibrary(void);
-//void updateMouse(void);
-//void clearMouse(void);
-//void drawMouse(void);
-
-
-//extern list_t windowList;
-//extern volatile int32_t mouseX;
-//extern volatile int32_t mouseY;
-//extern int32_t mouseXold;
-//extern int32_t mouseYold;
-
-
-// The functions below shouldn't really be here, anything using intutuion should be using the proper interface by now!!.
-//window_t* OpenWindow(window_t* parent,uint32_t x, uint32_t y, uint32_t w, uint32_t h,uint64_t flags,char* title);
-//void SetScreenTitle(window_t* window,char* title);
-//void MoveWindow(window_t* window,int32_t x, int32_t y);
-//void Focus(window_t* window);
-//void ResizeWindow(window_t*, uint32_t, uint32_t );
-
-//void IntuitionUpdate(void);
-//void RedrawWindow(window_t* window);
-
-//void WindowToBack(window_t* window);
-//void WindowToFront(window_t* window);
-//void PriorityOrderPrivate(window_t* window);
-
-
-// drawing functions
-//void Plot(window_t* window,uint32_t x, uint32_t y, uint32_t colour);
-//void PutChar(window_t*, uint32_t x, uint32_t y, uint8_t character, uint32_t fColour, uint32_t bColour);
-//void DrawRectangle(window_t* window,uint32_t x, uint32_t y, uint32_t w, uint32_t h, uint32_t colour);
 
 #endif /* intuition_h */
